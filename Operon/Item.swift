7cf7b0dc6b911bce31e3767dcd69202d4f3e2574
md. @crypto <105918255+Hqzdev@users.noreply.github.com>
2026-04-26 15:37:29 +0500

    Initial Commit

---
+++ Operon/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Operon
+//
+//  Created by Yaroslav Fairfieldd on 4/26/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Operon/OperonApp.swift
@@ -0,0 +1,32 @@
+//
+//  OperonApp.swift
+//  Operon
+//
+//  Created by Yaroslav Fairfieldd on 4/26/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OperonApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
